5fc1145bb467cd82d2a8f3259805e1b3a07e7123
Tiago Lemos Neitzke <tl.neitzke@hotmail.com>
2022-07-19 17:21:53 -0400

    implementando envio de email customizado

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Mail\MensagemTestMail;
 
 Route::get('/', function () {
     return view('welcome');
@@ -10,3 +11,9 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::resource('tarefa', 'App\Http\Controllers\TarefaController');
+
+Route::get('mensagem-teste', function(){
+    //return new MensagemTestMail();
+    Mail::to('tiagoeinez@gmail.com')->send(new MensagemTestMail()); // posso para debug, só executar esta linha de código no tinker, não esquecer de dar um use na class use App\Mail\MensagemTestMail;
+    return 'E-mail enviado com sucesso';
+});
